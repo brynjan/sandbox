26e5a0f3f946bdcee8fbacc21ce7ee02c93bf2db
Brynjar Norum <brynjar.norum@embriq.no>
2018-08-24 16:22:58 +0200

    intial version

---
+++ spring/spring-boot-mutual-tls/secureclient/src/test/java/no/progconsult/secureclient/SecureclientApplicationTests.java
@@ -0,0 +1,15 @@
+package no.progconsult.secureclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecureclientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
