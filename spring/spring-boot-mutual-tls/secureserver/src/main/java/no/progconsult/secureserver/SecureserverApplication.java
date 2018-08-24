64d89fc8ae3380d62abd30f3d48c28fbc8a1f646
Brynjar Norum <brynjar.norum@embriq.no>
2018-08-24 15:59:18 +0200

    Initial version

---
+++ spring/spring-boot-mutual-tls/secureserver/src/main/java/no/progconsult/secureserver/SecureserverApplication.java
@@ -0,0 +1,12 @@
+package no.progconsult.secureserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureserverApplication.class, args);
+    }
+}
