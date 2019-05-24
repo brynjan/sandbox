e27d3462902311e4fb72adfd8924a9f5ccdb6004
Brynjar Norum <brynjar.norum@rejlers.no>
2019-05-24 15:24:03 +0200

    initial

---
+++ aws/sqs-example/src/test/java/no/progconsult/aws/sqs/SqsClient.java
@@ -0,0 +1,18 @@
+package no.progconsult.aws.sqs;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:brynjar.norum@rejlers.no">Brynjar Norum</a> 2019-05-24.
+ */
+public class SqsClient {
+
+    @Test
+    public void testName() {
+
+
+
+
+
+    }
+}
