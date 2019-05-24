de3fcd59472ab9cf9df25160ef6546df2f029abd
Brynjar Norum <brynjar.norum@rejlers.no>
2019-05-24 15:42:28 +0200

    added bucket list

--- aws/sqs-example/src/test/java/no/progconsult/aws/sqs/SqsClient.java
+++ aws/sqs-example/src/test/java/no/progconsult/aws/sqs/SqsClient.java
@@ -1,5 +1,9 @@
 package no.progconsult.aws.sqs;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.testng.annotations.Test;
 
 /**
@@ -10,8 +14,22 @@ public class SqsClient {
     @Test
     public void testName() {
 
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                                                 .withRegion(Regions.EU_WEST_1)
+                                                 .build();
 
 
+        s3Client.listBuckets().stream().forEach(bucket -> System.out.println("bucket1: " + bucket.getName()));
+        System.out.println();
+
+
+
+        final ProfileCredentialsProvider credentialsProvider
+                = new ProfileCredentialsProvider("bno");
+        s3Client = AmazonS3ClientBuilder.standard().withCredentials(credentialsProvider).build();
+
+        s3Client.listBuckets().stream().forEach(bucket -> System.out.println("bucket2: " + bucket.getName()));
+        System.out.println();
 
 
     }
