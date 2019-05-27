68dae06201668b6574c55171ac1f9c1f95243cf5
Brynjar Norum <brynjar.norum@embriq.no>
2019-05-27 22:33:37 +0200

    sqs

--- aws/sqs-example/src/test/java/no/progconsult/aws/sqs/SqsClient.java
+++ aws/sqs-example/src/test/java/no/progconsult/aws/sqs/SqsClient.java
@@ -4,6 +4,9 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
 import org.testng.annotations.Test;
 
 /**
@@ -31,6 +34,26 @@ public class SqsClient {
         s3Client.listBuckets().stream().forEach(bucket -> System.out.println("bucket2: " + bucket.getName()));
         System.out.println();
 
+    }
 
+    @Test
+    public void send() throws Exception{
+
+        AmazonSQS amazonSQS = AmazonSQSClientBuilder.standard()
+                                              .withRegion(Regions.EU_WEST_1)
+                                              .withCredentials(new ProfileCredentialsProvider("bno"))
+                                              .build();
+
+
+        System.out.println("Creating a new SQS queue called MyQueue.\n");
+        final CreateQueueRequest createQueueRequest =
+                new CreateQueueRequest("test_bno");
+        final String myQueueUrl = amazonSQS.createQueue(createQueueRequest).getQueueUrl();
+
+        System.out.println("Listing all queues in your account.\n");
+        for (final String queueUrl : amazonSQS.listQueues().getQueueUrls()) {
+            System.out.println("  QueueUrl: " + queueUrl);
+        }
+        System.out.println();
     }
 }
