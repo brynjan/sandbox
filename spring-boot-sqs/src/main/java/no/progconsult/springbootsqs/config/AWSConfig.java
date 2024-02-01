f55384806beb957b9abc8de97e1c4186d0c63ddc
Brynjar Norum <brynjar.norum@embriq.no>
2024-02-01 14:00:39 +0100

    FR-1087 more setup

--- spring-boot-sqs/src/main/java/no/progconsult/springbootsqs/config/AWSConfig.java
+++ spring-boot-sqs/src/main/java/no/progconsult/springbootsqs/config/AWSConfig.java
@@ -1,19 +1,10 @@
 package no.progconsult.springbootsqs.config;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.regions.Regions;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 
 @Configuration
 public class AWSConfig {
-
 
 
 //    @Bean
--- spring-boot-sqs/src/test/java/no/progconsult/springbootsqs/AbstractSystemTest.java
+++ spring-boot-sqs/src/test/java/no/progconsult/springbootsqs/AbstractSystemTest.java
@@ -1,19 +1,12 @@
 package no.progconsult.springbootsqs;
 
-import com.amazonaws.services.sqs.model.Message;
-import com.amazonaws.services.sqs.model.MessageAttributeValue;
-import com.amazonaws.services.sqs.model.SendMessageRequest;
-import com.github.tomakehurst.wiremock.WireMockServer;
-import no.embriq.quant.flow.test.sqs.SqsAsyncClientMock;
-import no.embriq.quant.flow.test.sqs.SqsServerMock;
+//import no.embriq.quant.flow.test.sqs.SqsAsyncClientMock;
+
 import no.progconsult.springbootsqs.config.TestConfig;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.core.env.Environment;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
@@ -23,15 +16,12 @@ import org.testng.annotations.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.Map;
 
-import static org.mockito.Mockito.reset;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @SpringBootTest(classes = {SpringBootSqsMain.class, TestConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //Setup wiremock, dynamically http port.
-@AutoConfigureWireMock(port = 0)
+//@AutoConfigureWireMock(port = 0)
 @TestPropertySource("classpath:springbootsqs-test.properties")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class AbstractSystemTest extends AbstractTestNGSpringContextTests {
@@ -39,14 +29,14 @@ public abstract class AbstractSystemTest extends AbstractTestNGSpringContextTest
     private static final Logger LOG = getLogger(AbstractSystemTest.class);
 
     protected static final String sqsMeterreadingDeliveries = "embriq-nanometering-meterreading-deliveries-subscription";
-    @Autowired
-    protected SqsServerMock sqsServerMock;
+//    @Autowired
+//    protected SqsServerMock sqsServerMock;
 
-    @Autowired
-    protected SqsAsyncClientMock sqsClientMock;
-
-    @Autowired
-    protected WireMockServer wireMockServer;
+//    @Autowired
+//    protected SqsAsyncClientMock sqsClientMock;
+//
+//    @Autowired
+//    protected WireMockServer wireMockServer;
 
     @Autowired
     protected TestRestTemplate restTemplate;
@@ -54,8 +44,8 @@ public abstract class AbstractSystemTest extends AbstractTestNGSpringContextTest
 //    @Autowired
 //    protected Receiver receiverMock;
 
-    @LocalServerPort
-    protected int port;
+//    @LocalServerPort
+//    protected int port;
 
 //    @Autowired
 //    protected SimpleMessageListenerContainer simpleMessageListenerContainer;
@@ -82,8 +72,8 @@ public abstract class AbstractSystemTest extends AbstractTestNGSpringContextTest
 
     @BeforeMethod
     public void resetMocks() {
-        sqsClientMock.clearState();
-        wireMockServer.resetAll();
+//        sqsClientMock.clearState();
+//        wireMockServer.resetAll();
 //        reset(publisherMock);
     }
 
@@ -91,74 +81,74 @@ public abstract class AbstractSystemTest extends AbstractTestNGSpringContextTest
     @AfterMethod
     public void clearState() {
         try {
-            sqsClientMock.clearState();
-            wireMockServer.resetAll();
+//            sqsClientMock.clearState();
+//            wireMockServer.resetAll();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void setupSqsQueues(SqsAsyncClientMock sqsClientMock) {
-        //Queues must exists before the SimpleMessageListenerContainer is started. Receivers are not started on queues not
-        //found at the time the container starts.
-        sqsClientMock.createQueueIfNotExists(sqsMeterreadingDeliveries);
-    }
+//    public static void setupSqsQueues(SqsAsyncClientMock sqsClientMock) {
+//        //Queues must exists before the SimpleMessageListenerContainer is started. Receivers are not started on queues not
+//        //found at the time the container starts.
+//        sqsClientMock.createQueueIfNotExists(sqsMeterreadingDeliveries);
+//    }
 
-    protected String serverUrl() {
-        return String.format("http://localhost:%s", port);
-    }
+//    protected String serverUrl() {
+//        return String.format("http://localhost:%s", port);
+//    }
 
-    protected String basePathUrl() {
-        return serverUrl() + environment.getProperty("base.path");
-    }
+//    protected String basePathUrl() {
+//        return serverUrl() + environment.getProperty("base.path");
+//    }
 
 
-    public void waitUntilMessageDeleted(String queueName) {
-        sqsClientMock.waitUntilMessageDeleted(sqsClientMock.queueUrl(queueName));
-    }
+//    public void waitUntilMessageDeleted(String queueName) {
+//        sqsClientMock.waitUntilMessageDeleted(sqsClientMock.queueUrl(queueName));
+//    }
+//
+//    public void waitUntilMessageDeleted(String queueName, int count) {
+//        sqsClientMock.waitUntilMessageDeleted(sqsClientMock.queueUrl(queueName), count);
+//    }
+//
+//    private String getQueueUrl(String queue) {
+//        return sqsClientMock.getQueueUrl(queue).getQueueUrl();
+//    }
+//
+//    protected String send(String queue, String body) {
+//        SendMessageRequest messageRequest = new SendMessageRequest()
+//                .withQueueUrl(getQueueUrl(queue))
+//                .withMessageBody(body);
+//
+//        return sqsClientMock.sendMessage(messageRequest).getMessageId();
+//    }
+//
+//    protected String send(String queue, String body, Map<String, MessageAttributeValue> sqsMessageAttributes) {
+//        SendMessageRequest messageRequest = new SendMessageRequest()
+//                .withQueueUrl(getQueueUrl(queue))
+//                .withMessageBody(body)
+//                .withMessageAttributes(sqsMessageAttributes);
+//
+//        return sqsClientMock.sendMessage(messageRequest).getMessageId();
+//    }
+//
+//    public Message receiveSingleMessage(final String queueName) {
+//        return sqsClientMock.receiveSingleMessage(sqsClientMock.queueUrl(queueName));
+//    }
+//
+//    public List<Message> receiveMessages(String queueName, int numberOfExpectedMessages) {
+//        return sqsClientMock.receiveMessages(sqsClientMock.queueUrl(queueName), numberOfExpectedMessages);
+//    }
 
-    public void waitUntilMessageDeleted(String queueName, int count) {
-        sqsClientMock.waitUntilMessageDeleted(sqsClientMock.queueUrl(queueName), count);
-    }
-
-    private String getQueueUrl(String queue) {
-        return sqsClientMock.getQueueUrl(queue).getQueueUrl();
-    }
-
-    protected String send(String queue, String body) {
-        SendMessageRequest messageRequest = new SendMessageRequest()
-                .withQueueUrl(getQueueUrl(queue))
-                .withMessageBody(body);
-
-        return sqsClientMock.sendMessage(messageRequest).getMessageId();
-    }
-
-    protected String send(String queue, String body, Map<String, MessageAttributeValue> sqsMessageAttributes) {
-        SendMessageRequest messageRequest = new SendMessageRequest()
-                .withQueueUrl(getQueueUrl(queue))
-                .withMessageBody(body)
-                .withMessageAttributes(sqsMessageAttributes);
-
-        return sqsClientMock.sendMessage(messageRequest).getMessageId();
-    }
-
-    public Message receiveSingleMessage(final String queueName) {
-        return sqsClientMock.receiveSingleMessage(sqsClientMock.queueUrl(queueName));
-    }
-
-    public List<Message> receiveMessages(String queueName, int numberOfExpectedMessages) {
-        return sqsClientMock.receiveMessages(sqsClientMock.queueUrl(queueName), numberOfExpectedMessages);
-    }
-
-    public static String readFile(String filename) {
-        try (InputStream in = AbstractSystemTest.class.getClassLoader().getResourceAsStream(filename)) {
-            if (in == null) {
-                throw new RuntimeException(String.format("File=%s not found.", filename));
-            }
-            return IOUtils.toString(in, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    public static String readFile(String filename) {
+//        try (InputStream in = AbstractSystemTest.class.getClassLoader().getResourceAsStream(filename)) {
+//            if (in == null) {
+//                throw new RuntimeException(String.format("File=%s not found.", filename));
+//            }
+//            return IOUtils.toString(in, StandardCharsets.UTF_8);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
 }
