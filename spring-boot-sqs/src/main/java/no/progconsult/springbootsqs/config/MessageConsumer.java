d2c2b2d571a7dcdf555f23ac3aad91ea46044baa
Brynjar Norum <brynjar.norum@embriq.no>
2024-04-03 14:09:17 +0200

    FR-1087 cleanup

--- spring-boot-sqs/src/main/java/no/progconsult/springbootsqs/config/MessageConsumer.java
+++ spring-boot-sqs/src/main/java/no/progconsult/springbootsqs/config/MessageConsumer.java
@@ -1,23 +1,51 @@
 package no.progconsult.springbootsqs.config;
 
+import io.awspring.cloud.sqs.annotation.SqsListener;
+import io.awspring.cloud.sqs.listener.QueueMessageVisibility;
+import no.embriq.quant.flow.typelib.common.QFEvent;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.stereotype.Component;
 
-/**
- * Created by lcsontos on 8/4/17.
- */
-//@Component
+import java.util.Optional;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Component
 public class MessageConsumer {
+    private static final Logger LOG = getLogger(MessageConsumer.class);
 
-    private static final Logger LOG = LoggerFactory.getLogger(MessageConsumer.class);
+    private final SpringBootSqsRoute springBootSqsRoute;
+    private final SqsBackOff sqsBackOff;
 
-    @JmsListener(destination = "${sqs.queue.in}")
-    public void receive(@Payload String message) {
-        LOG.info("Received message {}.", message);
-        throw new RuntimeException("feil feil");
+
+    public MessageConsumer(SpringBootSqsRoute springBootSqsRoute, SqsBackOff sqsBackOff) {
+        this.springBootSqsRoute = springBootSqsRoute;
+        this.sqsBackOff = sqsBackOff;
+    }
+
+    @SqsListener(value = "embriq-volueagent-in-test")
+    public void receive(QFEvent message) {
+        // Process the received message
+        System.out.println("Received message. Size: " + message.getPayload().length());
+        System.out.println(StringUtils.abbreviate(message.getPayload(), 100_000));
+    }
+
+    @MessageExceptionHandler(Exception.class)
+    public void handleException(Exception exception, QueueMessageVisibility visibility,
+                                @Header("ReceiptHandle") String receiptHandle,
+                                @Header("ApproximateReceiveCount") String approximateReceiveCount) {
+
+        int nextVisibilityTimeoutSeconds = sqsBackOff.calculateNextVisibilityTimeoutSeconds(approximateReceiveCount);
+        visibility.changeToAsync(nextVisibilityTimeoutSeconds);
+
+        LOG.info("ChangeMessageVisibility nextVisibilityTimeout: {} seconds, receiptHandle: {}", nextVisibilityTimeoutSeconds,
+                receiptHandle);
+        LOG.error("Exception encountered while processing message. {}", exception.getMessage());
+        LOG.info("Stacktrace", exception);
     }
 
 }
+
